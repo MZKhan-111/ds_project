1e963f1380d6ab87b05e1cc1e5c96cdd3e470193
Md Moniruzzaman Khan <khanuttam83@gmail.com>
2025-01-20 12:39:14 -0800

    Added code in the common.py file

--- src/datascience/utils/common.py
+++ src/datascience/utils/common.py
@@ -1,52 +1,104 @@
 import os
 import yaml
-from src.datascience.utils import logger
+from src.datascience import logger
 import json
 import joblib
 from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
-from typing import Dict, Any, Union
+from typing import Any
 from box.exceptions import BoxValueError
 
 
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
-    """
-    Read yaml file
+    """reads yaml file and returns
+
     Args:
-        path_to_yaml (str): path to yaml file
+        path_to_yaml (str): path like input
 
     Raises:
-        valueError: if the file is empty
+        ValueError: if yaml file is empty
+        e: empty file
 
-    Return: 
-        ConfigBox object
+    Returns:
+        ConfigBox: ConfigBox type
     """
     try:
-        with open(path_to_yaml) as file:
-            content = yaml.safe_load(file)
-            logger.info(f"Reading the yaml file: {path_to_yaml}")
-        return ConfigBox(content)
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
     except BoxValueError:
-        logger.error(f"Empty file: {path_to_yaml}")
-        raise ValueError(f"Empty file: {path_to_yaml}")
+        raise ValueError("yaml file is empty")
     except Exception as e:
-        logger.error(f"Error reading the yaml file: {path_to_yaml}")
         raise e
-    
+        
+
 
 @ensure_annotations
-def create_directories(path_to_directory: list, verbose=True):
-    """
-    Create list of directories
+def create_directories(path_to_directories: list, verbose=True):
+    """create list of directories
+
     Args:
-        path_to_directory (list): list of path to directory
-        ignore_log (bool, optional): ignore if multiple directories are created. Defaults to True.
+        path_to_directories (list): list of path of directories
+        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
     """
-    try:
-        os.makedirs(path_to_directory, exist_ok=True)
-        logger.info(f"Creating directory: {path_to_directory}")
-    except Exception as e:
-        logger.error(f"Error creating directory: {path_to_directory}")
-        raise e
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"created directory at: {path}")
+
+@ensure_annotations
+def save_json(path: Path, data: dict):
+    """save json data
+
+    Args:
+        path (Path): path to json file
+        data (dict): data to be saved in json file
+    """
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
+
+    logger.info(f"json file saved at: {path}")
+
+@ensure_annotations
+def load_json(path: Path) -> ConfigBox:
+    """load json files data
+
+    Args:
+        path (Path): path to json file
+
+    Returns:
+        ConfigBox: data as class attributes instead of dict
+    """
+    with open(path) as f:
+        content = json.load(f)
+
+    logger.info(f"json file loaded succesfully from: {path}")
+    return ConfigBox(content)
+
+@ensure_annotations
+def save_bin(data: Any, path: Path):
+    """save binary file
+
+    Args:
+        data (Any): data to be saved as binary
+        path (Path): path to binary file
+    """
+    joblib.dump(value=data, filename=path)
+    logger.info(f"binary file saved at: {path}")
+
+@ensure_annotations
+def load_bin(path: Path) -> Any:
+    """load binary data
+
+    Args:
+        path (Path): path to binary file
+
+    Returns:
+        Any: object stored in the file
+    """
+    data = joblib.load(path)
+    logger.info(f"binary file loaded from: {path}")
+    return data
